a6e58dce697e358b7f78ef5b9852f89e985981b1
G7495x <G7495x@gmail.com>
2020-10-26 19:22:09 -0700

    v0.0.8

---
+++ precache-manifest.c1c48bf9e64ad740628cf073604e9094.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4e8ab2919b9133b389b2a9350d86b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2a2a2dbffe57e4e2c85",
+    "url": "./static/css/main.4e7d7e95.chunk.css"
+  },
+  {
+    "revision": "a9089b6dcccf2342603c",
+    "url": "./static/js/2.bdf49e96.chunk.js"
+  },
+  {
+    "revision": "33068e4a9f5685e528be5e17653bc86f",
+    "url": "./static/js/2.bdf49e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a2a2dbffe57e4e2c85",
+    "url": "./static/js/main.e65849df.chunk.js"
+  },
+  {
+    "revision": "c81f63ff18a64e0f8cd3",
+    "url": "./static/js/runtime-main.3b26157f.js"
+  },
+  {
+    "revision": "da08a5a2da596d573669d1db85f17818",
+    "url": "./static/media/caret-down.da08a5a2.svg"
+  },
+  {
+    "revision": "c46b111890cceb4b27ea2f2a11c36c3a",
+    "url": "./static/media/check.c46b1118.svg"
+  },
+  {
+    "revision": "507598d7a972f17a5c98737aca0b73c2",
+    "url": "./static/media/circle.507598d7.svg"
+  },
+  {
+    "revision": "74d7f9737f33db0ffe58bcb60aa7f61d",
+    "url": "./static/media/letter.74d7f973.png"
+  },
+  {
+    "revision": "1dab0eed2834b4940d588c2d96d382e5",
+    "url": "./static/media/mfglabsiconset-webfont.1dab0eed.svg"
+  },
+  {
+    "revision": "b0a0976ce87a47700c6e689226e7a411",
+    "url": "./static/media/mfglabsiconset-webfont.b0a0976c.eot"
+  },
+  {
+    "revision": "ef329adb46ed36028ac928befa3a17c8",
+    "url": "./static/media/mfglabsiconset-webfont.ef329adb.woff"
+  },
+  {
+    "revision": "f130ab32e5da85a090f22822ddec64e6",
+    "url": "./static/media/mfglabsiconset-webfont.f130ab32.ttf"
+  },
+  {
+    "revision": "098550cd999fec17af099a4ac4415af9",
+    "url": "./static/media/search.098550cd.svg"
+  },
+  {
+    "revision": "3d035a553db5cc6235c369217aa9435f",
+    "url": "./static/media/spinner.3d035a55.gif"
+  },
+  {
+    "revision": "a5a8aa617fc4e4f062fc0b9056d6b967",
+    "url": "./static/media/tab-start.a5a8aa61.svg"
+  },
+  {
+    "revision": "4688d215ed64bb986d4888a6abe242d6",
+    "url": "./static/media/triangle-down.4688d215.svg"
+  }
+]);
